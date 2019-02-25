1391924c3a24cc9789875d46e231e01eafc91cc8
lightjameslyy <lightjameslyy@gmail.com>
2019-02-25 16:13:05 +0800

    test if hyper-threading is on

---
+++ test/20190225-test-hyper-threading/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream> /* cout */
+#include <unistd.h>/* gethostname */
+#include <netdb.h> /* struct hostent */
+#include <arpa/inet.h> /* inet_ntop */
+#include <stdlib.h> /* system */
+
+bool GetHostInfo(std::string& hostName, std::string& Ip) {
+    char name[256];
+    gethostname(name, sizeof(name));
+    hostName = name;
+
+    struct hostent* host = gethostbyname(name);
+    char ipStr[32];
+    const char* ret = inet_ntop(host->h_addrtype, host->h_addr_list[0], ipStr, sizeof(ipStr));
+    if (NULL==ret) {
+        std::cout << "hostname transform to ip failed";
+        return false;
+    }
+    Ip = ipStr;
+    return true;
+}
+int main(int argc, char *argv[]) {
+    std::string hostName;
+    std::string Ip;
+
+    bool ret = GetHostInfo(hostName, Ip);
+    if (true == ret) {
+        std::cout << "hostname: " << hostName << std::endl;
+        std::cout << "Ip: " << Ip << std::endl;
+    }
+    system("lscpu");
+    return 0;
+}
