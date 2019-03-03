fcfa47003e5a4bf10aecf5ccabaede068d5b5aa3
lightjameslyy <lightjameslyy@gmail.com>
2019-03-03 18:06:12 +0800

    test environ in unistd.h

---
+++ test/20190303-test-extern-environ/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+extern char **environ;
+int main(int argc, char *argv[]) {
+    char **p = environ;
+    while (*p != NULL) {
+        printf("%s (%p)\n", *p, *p);
+        *p++;
+    }
+    return 0;
+}
