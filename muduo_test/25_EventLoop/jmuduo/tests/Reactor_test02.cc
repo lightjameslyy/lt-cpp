1a8c8242e27b588d8c6cf2a94aabae52b1e7882a
lightjameslyy <lightjameslyy@gmail.com>
2019-03-28 11:26:49 +0800

    muduo_net: simplified EventLoop

---
+++ muduo_test/25_EventLoop/jmuduo/tests/Reactor_test02.cc
@@ -0,0 +1,25 @@
+﻿#include <muduo/net/EventLoop.h>
+
+#include <stdio.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+EventLoop* g_loop;//全局变量所有线程都可以访问到
+
+void threadFunc()
+{
+	g_loop->loop();//执行的线程和初始化的线程不一样
+}
+
+int main(void)
+{
+	EventLoop loop;
+	g_loop = &loop;
+	Thread t(threadFunc);
+	t.start();
+	//loop.loop();
+	t.join();
+	return 0;
+}
+
