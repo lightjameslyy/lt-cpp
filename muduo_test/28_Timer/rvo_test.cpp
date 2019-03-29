e8a19771a880261a69f48b78a2a6775af6c8810b
lightjameslyy <lightjameslyy@gmail.com>
2019-03-29 14:32:07 +0800

    muduo_net: TimerId, Timer, TimerQueue

---
+++ muduo_test/28_Timer/rvo_test.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Foo   
+{   
+	Foo() { cout << "Foo ctor" << endl; }
+	Foo(const Foo&) { cout << "Foo copy ctor" << endl; }
+	void operator=(const Foo&) { cout << "Foo operator=" << endl; }
+	~Foo() { cout << "Foo dtor" << endl; }
+};  
+
+Foo make_foo()   
+{   
+	Foo f;//构造函数
+	return f;//如果没有rvo优化的话，这里返回要调用拷贝构造函数，rvo优化直接把该对象返回回去，相当于这个对象被提升了
+				//提升成不是一个局部对象，vs debug底下没有rvo优化,release就有rvo优化
+	//return Foo();  
+}
+  
+int main(void)
+{
+	make_foo();
+	return 0;
+}
---
+++ muduo_test/28_Timer/set_test.cpp
@@ -0,0 +1,14 @@
+﻿#include <set>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a[] = { 1, 2, 3, 4, 5};
+	set<int> s(a, a+5);//自动排序
+
+	cout<<*s.lower_bound(2)<<endl;//返回第一个值大于等于2的位置
+	cout<<*s.upper_bound(2)<<endl;//返回第一个值大于2的位置
+	return 0;
+}
