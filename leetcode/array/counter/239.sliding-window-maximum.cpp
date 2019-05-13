490923dc49b0a4af4b35602f1c97b86f64e769ec
lightjameslyy <lightjameslyy@gmail.com>
2019-05-13 14:35:11 +0000

    239 solved: monotonic queue

--- leetcode/array/counter/239.sliding-window-maximum.cpp
+++ leetcode/array/counter/239.sliding-window-maximum.cpp
@@ -15,14 +15,14 @@
  * from the very left of the array to the very right. You can only see the k
  * numbers in the window. Each time the sliding window moves right by one
  * position. Return the max sliding window.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
- * Output: [3,3,5,5,6,7] 
- * Explanation: 
- * 
+ * Output: [3,3,5,5,6,7]
+ * Explanation:
+ *
  * Window position                Max
  * ---------------               -----
  * [1  3  -1] -3  5  3  6  7       3
@@ -31,18 +31,50 @@
  * ⁠1  3  -1 [-3  5  3] 6  7       5
  * ⁠1  3  -1  -3 [5  3  6] 7       6
  * ⁠1  3  -1  -3  5 [3  6  7]      7
- * 
- * 
- * Note: 
+ *
+ *
+ * Note:
  * You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty
  * array.
- * 
+ *
  * Follow up:
  * Could you solve it in linear time?
  */
+
+/*
+ * 思路：Monotonic Queue
+ * deque
+ */
 class Solution {
-public:
+  public:
+    deque<pair<int, int>> mono_que; // pair<id, val>
+
+    void push(int id, int val) {
+        // pop_back values less than val 
+        for (; !mono_que.empty() && mono_que.back().second < val ; mono_que.pop_back());
+        mono_que.push_back(make_pair(id, val));
+    }
+
+    void pop(int id) {
+        // pop the out-dated entry
+        if (!mono_que.empty() && mono_que.front().first == id)
+            mono_que.pop_front();
+    }
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
+        vector<int> res;
+        int size = nums.size();
+        if (size == 0)
+            return res;
+        for (int i = 0; i < k; ++i) {
+            push(i, nums[i]);
+        }
+        res.push_back(mono_que.front().second);
+        for (int i = k; i < size; ++i) {
+            pop(i-k);
+            push(i, nums[i]);
+            res.push_back(mono_que.front().second);
+        }
+        return res;
     }
 };
