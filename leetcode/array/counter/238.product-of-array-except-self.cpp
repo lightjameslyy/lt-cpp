9116d207e872ec68ef199ab0bb9cc181ca489660
lightjameslyy <lightjameslyy@gmail.com>
2019-05-16 23:34:39 +0800

    238 need test

--- leetcode/array/counter/238.product-of-array-except-self.cpp
+++ leetcode/array/counter/238.product-of-array-except-self.cpp
@@ -29,9 +29,31 @@
  * not count as extra space for the purpose of space complexity analysis.)
  * 
  */
+
+/*
+ * 思路：双向计算
+ * 对每个res[i]，先算nums[0]*nums[1]*...*nums[i-1]，再算nums[i+1]*nums[i+2]*...*nums[size-1]
+ */
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
+        int size = nums.size();
+        vector<int> res(size, 1);
+
+        int tmp = 1;
+        // left side
+        for (int i = 1; i < size; ++i) {
+            tmp *= nums[i-1];
+            res[i] = tmp;
+        }
+
+        tmp = 1;
+        // right side
+        for (int i = size-2; i >= 0; --i) {
+            tmp *= nums[i+1];
+            res[i] *= tmp;
+        }
+
+        return res;
     }
 };
