a5c77643d0d61c274120dd49b12798768b0a0062
lightjameslyy <lightjameslyy@gmail.com>
2019-10-31 11:47:25 +0800

    leetcode palindrome series generated

---
+++ leetcode-clion/string/palindrome/267.palindrome-permutation-ii.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=267 lang=cpp
+ *
+ * [267] Palindrome Permutation II
+ *
+ * https://leetcode.com/problems/palindrome-permutation-ii/description/
+ *
+ * algorithms
+ * Medium (34.70%)
+ * Total Accepted:    30.2K
+ * Total Submissions: 87.1K
+ * Testcase Example:  '"aabb"'
+ *
+ * Given a string s, return all the palindromic permutations (without
+ * duplicates) of it. Return an empty list if no palindromic permutation could
+ * be form.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "aabb"
+ * Output: ["abba", "baab"]
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "abc"
+ * Output: []
+ * 
+ */
+class Solution {
+public:
+    vector<string> generatePalindromes(string s) {
+        
+    }
+};
