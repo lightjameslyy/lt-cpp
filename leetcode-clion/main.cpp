8189e89718f566ca3fe6abe565ba434e4ecd322a
lightjameslyy <lightjameslyy@gmail.com>
2019-11-03 00:00:01 +0800

    leetcode 267: solved

--- leetcode-clion/main.cpp
+++ leetcode-clion/main.cpp
@@ -5,22 +5,22 @@
 
 #include "utils.h"
 
-#include "string/palindrome/132.palindrome-partitioning-ii.cpp"
+#include "string/palindrome/267.palindrome-permutation-ii.cpp"
 
 using namespace std;
 
 int main() {
     Solution s;
     vector<string> ss = {
-            "aab",
-            "aababac",
             "",
-            "a",
-            "aa",
-            "apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp"
+            "abc",
+            "aabb",
+            "abacaabcc"
     };
     for (int i = 0; i < ss.size(); ++i) {
-        cout << s.minCut(ss[i]) << endl;
+        //s.generatePalindromes(ss[i]);
+        print(s.generatePalindromes(ss[i]));
+        cout << endl;
     }
     return 0;
 }
--- leetcode-clion/string/palindrome/267.palindrome-permutation-ii.cpp
+++ leetcode-clion/string/palindrome/267.palindrome-permutation-ii.cpp
@@ -28,9 +28,50 @@
  * Output: []
  * 
  */
+
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
+    void helper(const string &s, string &center, string cur, int len, vector<string> &res) {
+        if (cur.length() == len) {
+            res.push_back(cur + center + string(cur.rbegin(), cur.rend()));
+            return;
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            cur += s[i];
+            helper(s.substr(0, i) + s.substr(i + 1), center, cur, len, res);
+            cur.pop_back();
+            // NOTE: take care of duplicates
+            while (i + 1 < s.length() && s[i] == s[i + 1])
+                ++i;
+        }
+    }
+
+    // backtracking
+    // 0ms, 8.7MB, 100%
     vector<string> generatePalindromes(string s) {
-        
+        int len = s.length();
+        vector<string> res;
+        string center = "";
+        // validate
+        int charCount[128] = {0};
+        for (char c : s)
+            charCount[c]++;
+        string minLeft = "";
+        for (int i = 0; i < 128; ++i) {
+            if (charCount[i] % 2) {
+                center.append(1, i);
+            }
+            if (center.length() > 1)
+                return res;
+            minLeft.append(charCount[i] / 2, i);
+        }
+        // generate left side permutations from minLeft to maxLeft
+        helper(minLeft, center, "", minLeft.length(), res);
+
+        return res;
     }
 };
