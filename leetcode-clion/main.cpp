f5f1d8cd3f75163e0094de1c00e471d2b12f72cf
lightjameslyy <lightjameslyy@gmail.com>
2019-11-06 23:16:41 +0800

    leetcode 258: solved

--- leetcode-clion/main.cpp
+++ leetcode-clion/main.cpp
@@ -5,23 +5,13 @@
 
 #include "utils.h"
 
-#include "math/basic/8.string-to-integer-atoi.cpp"
+#include "math/basic/258.add-digits.cpp"
 
 using namespace std;
 
 int main() {
     Solution sol;
-    vector<string> s = {
-            "42",
-            "    -42",
-            "4193 aejofi josdf",
-            "uhajf iasj 987",
-            "-9283758234765234",
-            "2147483646"
-    };
-    for (int i = 0; i < s.size(); ++i) {
-        cout << sol.myAtoi(s[i]) << endl;
-    }
+    cout << sol.addDigits(38) << endl;
 
     return 0;
 }
