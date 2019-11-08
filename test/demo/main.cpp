55597a3b23bd6b8f2241b66402ba419dd3d41704
lightjameslyy <lightjameslyy@gmail.com>
2019-11-08 14:14:27 +0800

    add demo

---
+++ test/demo/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <stdio.h>
+#include <chrono>
+
+#include "tools.h"
+
+using namespace std;
+using namespace chrono;
+
+int main(int argc, char* argv[]) {
+
+    string hostName, Ip;
+    if (GetHostInfo(hostName, Ip)) {
+    }
+    cout << "hostname: " << hostName << ", ip: " << Ip << endl;
+
+    auto start = system_clock::now();
+
+    printf("hello world.\n");
+
+    auto end = system_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "time: " << double(duration.count()) * microseconds::period::num / microseconds::period::den << "s\n";
+    return 0;
+}
