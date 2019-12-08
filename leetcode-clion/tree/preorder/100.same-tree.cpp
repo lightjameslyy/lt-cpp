4f1093de2c607fd1c6ecb512c270642f6006344e
lightjameslyy <lightjameslyy@gmail.com>
2019-12-08 15:57:51 +0800

    leetcode 100: solved

---
+++ leetcode-clion/tree/preorder/100.same-tree.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=100 lang=cpp
+ *
+ * [100] Same Tree
+ *
+ * https://leetcode.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (51.04%)
+ * Total Accepted:    434.7K
+ * Total Submissions: 851.7K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * Given two binary trees, write a function to check if they are the same or
+ * not.
+ * 
+ * Two binary trees are considered the same if they are structurally identical
+ * and the nodes have the same value.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ * 
+ * ⁠       [1,2,3],   [1,2,3]
+ * 
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         /           \
+ * ⁠        2             2
+ * 
+ * ⁠       [1,2],     [1,null,2]
+ * 
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ * 
+ * ⁠       [1,2,1],   [1,1,2]
+ * 
+ * Output: false
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    // 4ms, 10MB, 58.97%
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        queue<TreeNode *> pp, qq;
+        pp.push(p);
+        qq.push(q);
+        while (!pp.empty()) {
+            p = pp.front();
+            pp.pop();
+            q = qq.front();
+            qq.pop();
+            if (p == NULL && q == NULL) continue;
+            if ((p == NULL && q != NULL) || (p != NULL && q == NULL) || p->val != q->val)
+                return false;
+            pp.push(p->left);
+            pp.push(p->right);
+            qq.push(q->left);
+            qq.push(q->right);
+        }
+        return qq.empty();
+    }
+};
