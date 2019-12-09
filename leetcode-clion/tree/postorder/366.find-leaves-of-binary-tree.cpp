2315add702cc68a983118dd7191c08c4be39335b
lightjameslyy <lightjameslyy@gmail.com>
2019-12-09 13:30:21 +0800

    leetcode 366: solved

---
+++ leetcode-clion/tree/postorder/366.find-leaves-of-binary-tree.cpp
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=366 lang=cpp
+ *
+ * [366] Find Leaves of Binary Tree
+ *
+ * https://leetcode.com/problems/find-leaves-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (67.95%)
+ * Total Accepted:    54.9K
+ * Total Submissions: 80.8K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a binary tree, collect a tree's nodes as if you were doing this:
+ * Collect and remove all leaves, repeat until the tree is empty.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * 
+ * 1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * Output: [[4,5,3],[2],[1]]
+ * 
+ * 
+ * 
+ * 
+ * Explanation:
+ * 
+ * 1. Removing the leaves [4,5,3] would result in this tree:
+ * 
+ * 
+ * ⁠         1
+ * ⁠        / 
+ * ⁠       2          
+ * 
+ * 
+ * 
+ * 
+ * 2. Now removing the leaf [2] would result in this tree:
+ * 
+ * 
+ * ⁠         1          
+ * 
+ * 
+ * 
+ * 
+ * 3. Now removing the leaf [1] would result in the empty tree:
+ * 
+ * 
+ * ⁠         []         
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ */
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    int getHeight(TreeNode *root, vector<vector<int>> &res) {
+        if (root == NULL) return 0;
+        int lh = getHeight(root->left, res), rh = getHeight(root->right, res);
+        int h = max(lh, rh) + 1;
+        if (res.size() < h) {
+            res.push_back(vector<int>());
+        }
+        res[h - 1].push_back(root->val);
+        return h;
+    }
+
+    // 0ms, 9.6MB, 100%
+    vector<vector<int>> findLeaves(TreeNode *root) {
+        vector<vector<int>> res;
+        getHeight(root, res);
+        return res;
+    }
+};
