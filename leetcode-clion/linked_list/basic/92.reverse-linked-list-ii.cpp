214de52c427f23e54a182b843f224f58615713a2
lightjameslyy <lightjameslyy@gmail.com>
2019-12-10 16:18:33 +0800

    leetcode 92: solved

---
+++ leetcode-clion/linked_list/basic/92.reverse-linked-list-ii.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=92 lang=cpp
+ *
+ * [92] Reverse Linked List II
+ *
+ * https://leetcode.com/problems/reverse-linked-list-ii/description/
+ *
+ * algorithms
+ * Medium (36.41%)
+ * Total Accepted:    222.5K
+ * Total Submissions: 611.1K
+ * Testcase Example:  '[1,2,3,4,5]\n2\n4'
+ *
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ * 
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ */
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+
+    ListNode *reverseBetween(ListNode *head, int m, int n) {
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *pre = dummy;
+        for (int i = 0; i < m - 1; ++i)
+            pre = pre->next;
+        ListNode *post = pre->next;
+        for (int i = m; i < n; ++i) {
+            head = post->next;
+            post->next = head->next;
+            head->next = pre->next;
+            pre->next = head;
+        }
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
