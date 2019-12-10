aaf228d54163364abdb6d340d35baf1fc9f21216
lightjameslyy <lightjameslyy@gmail.com>
2019-12-10 18:19:22 +0800

    leetcode 21: solved

---
+++ leetcode-clion/linked_list/basic/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (49.64%)
+ * Total Accepted:    728.4K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *pre = dummy;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                pre->next = l1;
+                l1 = l1->next;
+            } else {
+                pre->next = l2;
+                l2 = l2->next;
+            }
+            pre = pre->next;
+        }
+        if (l1)
+            pre->next = l1;
+        if (l2)
+            pre->next = l2;
+        l1 = dummy->next;
+        delete dummy;
+        return l1;
+    }
+};
