0c938ba948b9686ab7ef192f68037d724c91dc10
lightjameslyy <lightjameslyy@gmail.com>
2019-12-10 10:26:29 +0800

    leetcode 285: solved

---
+++ leetcode-clion/tree/bst/285.inorder-successor-in-bst.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=285 lang=cpp
+ *
+ * [285] Inorder Successor in BST
+ *
+ * https://leetcode.com/problems/inorder-successor-in-bst/description/
+ *
+ * algorithms
+ * Medium (36.84%)
+ * Total Accepted:    122.1K
+ * Total Submissions: 331.5K
+ * Testcase Example:  '[2,1,3]\n1'
+ *
+ * Given a binary search tree and a node in it, find the in-order successor of
+ * that node in the BST.
+ * 
+ * The successor of a node p is the node with the smallest key greater than
+ * p.val.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [2,1,3], p = 1
+ * Output: 2
+ * Explanation: 1's in-order successor node is 2. Note that both p and the
+ * return value is of TreeNode type.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], p = 6
+ * Output: null
+ * Explanation: There is no in-order successor of the current node, so the
+ * answer is null.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * If the given node has no in-order successor in the tree, return null.
+ * It's guaranteed that the values of the tree are unique.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ */
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    TreeNode *inorderSuccessor(TreeNode *root, TreeNode *p) {
+        if (!root) return 0;
+        stack<TreeNode *> s;
+        bool found = false;
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+        while (!s.empty()) {
+            root = s.top();
+            if (found == true)
+                return root;
+            if (root == p)
+                found = true;
+            s.pop();
+            root = root->right;
+            while (root) {
+                s.push(root);
+                root = root->left;
+            }
+        }
+        return NULL;
+    }
+};
