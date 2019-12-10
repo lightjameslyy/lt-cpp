09de5046d541b20246d75821730ca29818ed92cf
lightjameslyy <lightjameslyy@gmail.com>
2019-12-10 09:35:26 +0800

    leetcode 109: solved. mark

---
+++ leetcode-clion/tree/bst/109.convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=109 lang=cpp
+ *
+ * [109] Convert Sorted List to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (43.16%)
+ * Total Accepted:    199.3K
+ * Total Submissions: 461.8K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given a singly linked list where elements are sorted in ascending order,
+ * convert it to a height balanced BST.
+ * 
+ * For this problem, a height-balanced binary tree is defined as a binary tree
+ * in which the depth of the two subtrees of every node never differ by more
+ * than 1.
+ * 
+ * Example:
+ * 
+ * 
+ * Given the sorted linked list: [-10,-3,0,5,9],
+ * 
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following
+ * height balanced BST:
+ * 
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ */
+/**
+ * Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ */
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    TreeNode *sortedListToBST(ListNode *head) {
+        if (!head) return NULL;
+        if (!head->next) return new TreeNode(head->val);
+        ListNode *left = head;
+        ListNode *mid = head;
+        ListNode *right = head;
+        while (right->next && right->next->next) {
+            left = mid;
+            mid = mid->next;
+            right = right->next->next;
+        }
+        right = mid->next;
+        left->next = NULL;
+        TreeNode *root = new TreeNode(mid->val);
+        if (mid != head)
+            root->left = sortedListToBST(head);
+        root->right = sortedListToBST(right);
+        return root;
+    }
+};
