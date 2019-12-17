b0f880681935b2429107668ce088d80746977f2c
lightjameslyy <lightjameslyy@gmail.com>
2019-12-17 23:30:15 +0800

    leetcode 35: solved

---
+++ leetcode-clion/binary_search/35.search-insert-position.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (41.25%)
+ * Total Accepted:    473K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ * 
+ * 
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        int size = nums.size();
+        if (!size) return 0;
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        if (right < 0) return 0;
+        if (left >= size) return size;
+        return (nums[left] >= target ? left : left + 1);
+    }
+};
