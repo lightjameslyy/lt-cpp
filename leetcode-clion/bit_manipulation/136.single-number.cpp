1104312f5e39163ea2ddca074c2064f2f6445472
lightjameslyy <lightjameslyy@gmail.com>
2019-12-21 15:32:12 +0800

    leetcode 136: solved

---
+++ leetcode-clion/bit_manipulation/136.single-number.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (61.86%)
+ * Total Accepted:    555.8K
+ * Total Submissions: 898.5K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,1]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * 
+ * 
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int> &nums) {
+        int res = 0;
+        for (int n : nums)
+            res ^= n;
+        return res;
+    }
+};
