7c757a6fe4a05016ca3c6775b2e91f8c4b18a196
lightjameslyy <lightjameslyy@gmail.com>
2019-12-21 16:30:58 +0800

    leetcode 207: solved

---
+++ leetcode-clion/topological_sort/207.course-schedule.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=207 lang=cpp
+ *
+ * [207] Course Schedule
+ *
+ * https://leetcode.com/problems/course-schedule/description/
+ *
+ * algorithms
+ * Medium (39.75%)
+ * Total Accepted:    276.3K
+ * Total Submissions: 695K
+ * Testcase Example:  '2\n[[1,0]]'
+ *
+ * There are a total of n courses you have to take, labeled from 0 to n-1.
+ * 
+ * Some courses may have prerequisites, for example to take course 0 you have
+ * to first take course 1, which is expressed as a pair: [0,1]
+ * 
+ * Given the total number of courses and a list of prerequisite pairs, is it
+ * possible for you to finish all courses?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2, [[1,0]] 
+ * Output: true
+ * Explanation: There are a total of 2 courses to take. 
+ * To take course 1 you should have finished course 0. So it is possible.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2, [[1,0],[0,1]]
+ * Output: false
+ * Explanation: There are a total of 2 courses to take. 
+ * To take course 1 you should have finished course 0, and to take course 0 you
+ * should
+ * also have finished course 1. So it is impossible.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The input prerequisites is a graph represented by a list of edges, not
+ * adjacency matrices. Read more about how a graph is represented.
+ * You may assume that there are no duplicate edges in the input
+ * prerequisites.
+ * 
+ * 
+ */
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+        if (numCourses < 2) return true;
+        vector<int> depsCount(numCourses, 0);
+        unordered_map<int, vector<int>> graph;
+        for (auto &edge : prerequisites) {
+            graph[edge[1]].push_back(edge[0]);
+            depsCount[edge[0]]++;
+        }
+        while (!graph.empty()) {
+            int oldSize = graph.size();
+            for (auto it = graph.begin(); it != graph.end();) {
+                if (!depsCount[it->first]) {
+                    for (int child : it->second) {
+                        if (!depsCount[child])
+                            return false;
+                        depsCount[child]--;
+                    }
+                    it = graph.erase(it);
+                } else
+                    ++it;
+            }
+            // has cycle
+            if (oldSize == graph.size())
+                return false;
+        }
+        return true;
+    }
+};
