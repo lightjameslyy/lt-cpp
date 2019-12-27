b2f89e734184a7938a9e630249032eebee0bb4e2
lightjameslyy <lightjameslyy@gmail.com>
2019-12-27 14:53:47 +0800

    update

--- test/20191212-mem-4k-align/main.cpp
+++ test/20191212-mem-4k-align/main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <omp.h>
 #include <vector>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 #include <chrono>
 #include <cassert>
 #include <cstring>
 
 #include "tools.h"
 
-#define ALIGNED_BASE 0x1000
-
 using namespace std;
 using namespace chrono;
-
-static void* alloc_4k_aligned_mem(size_t size, void** ppOut) {
-    *ppOut = (void*) calloc(1, size + ALIGNED_BASE - 1);
-    printf("ppOut: %p\n", *ppOut);
-    if(!ppOut) {
-        return NULL;
-    }
-
-    void* pAligned = (void*)((uintptr_t)(*ppOut + ALIGNED_BASE - 1) & ~(ALIGNED_BASE - 1));
-    printf("pAligned: %p\n", pAligned);
-    return pAligned;
-}
 
 int main(int argc, char* argv[]) {
 
@@ -32,16 +19,20 @@ int main(int argc, char* argv[]) {
     }
     cout << "hostname: " << hostName << ", ip: " << Ip << endl;
 
-    void* orig;
     size_t dataSize = 1LL << 27;
     cout << "data size: " << to_string(dataSize * sizeof(long long) / (double)(1LL << 30)) << "GB" << endl;
 
-    long long* data_aligned = (long long*) alloc_4k_aligned_mem(dataSize * sizeof(long long), &orig);
-    assert(orig);
-    long long* data_not_aligned = (long long*)orig;
+    //long long* data_aligned = (long long*) alloc_4k_aligned_mem(dataSize * sizeof(long long), &orig);
+    long long* data_aligned;
+    int ret = posix_memalign((void**)&data_aligned, 64, dataSize * sizeof(long long));
+    if (ret) {
+        fprintf(stderr, "posix_memalign failed: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "data_aligned: " << data_aligned << endl;
 
     for (int n = 0; n < 4; ++n) {
-#ifdef LT_TEST_ALIGNED
         auto start = system_clock::now();
 
         //memset(data_aligned, 0, dataSize * sizeof(long long));
@@ -52,8 +43,17 @@ int main(int argc, char* argv[]) {
         auto end = system_clock::now();
         auto duration = duration_cast<microseconds>(end - start);
         cout << "4k aligned time: " << double(duration.count()) * microseconds::period::num / microseconds::period::den << "s\n";
-#else
+    }
 
+    long long* data_not_aligned = (long long*)malloc(dataSize * sizeof(long long));
+    if (data_not_aligned == nullptr) {
+        fprintf(stderr, "malloc failed ...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "data_not_aligned: " << data_not_aligned << endl;
+
+    for (int n = 0; n < 4; ++n) {
         auto start = system_clock::now();
 
         //memset(data_not_aligned, 0, dataSize * sizeof(long long));
@@ -64,9 +64,9 @@ int main(int argc, char* argv[]) {
         auto end = system_clock::now();
         auto duration = duration_cast<microseconds>(end - start);
         cout << "4k not aligned time: " << double(duration.count()) * microseconds::period::num / microseconds::period::den << "s\n";
-#endif  // LT_TEST_ALIGNED
     }   // for n
 
-    free(orig);
+    free(data_aligned);
+    free(data_not_aligned);
     return 0;
 }
