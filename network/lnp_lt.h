3ce7e8cb40155dc0cef5555d1bd0bfd3ebae6a59
lightjameslyy <lightjameslyy@gmail.com>
2020-01-26 21:31:29 +0800

    update

--- network/lnp_lt.h
+++ network/lnp_lt.h
@@ -3,6 +3,7 @@
  */
 
 #include <cstdio>
+#include <arpa/inet.h>
 
 bool isHostBigEndian();
 
--- network/test/test.cpp
+++ network/test/test.cpp
@@ -4,3 +4,24 @@
 TEST(lnp, isHostBigEndian) {
     isHostBigEndian();
 }
+
+TEST(lnp, byteOrderConversion) {
+    unsigned int x = 0x12345678;
+
+    unsigned int y = htonl(x);
+    unsigned char* p = (unsigned char*)&y;
+    ASSERT_EQ(y, 0x78563412);
+
+    x = ntohl(y);
+    ASSERT_EQ(x, 0x12345678);
+}
+
+TEST(lnp, addrConversion) {
+    unsigned int addr = inet_addr("192.168.0.100");
+    printf("addr=%u\n", ntohl(addr));
+
+    struct in_addr ipaddr;
+    ipaddr.s_addr = addr;
+    printf("%s\n", inet_ntoa(ipaddr));
+}
+
