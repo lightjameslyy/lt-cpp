43eed39f5135843a660a371b2a8087f5f9a0669b
lightjameslyy <lightjameslyy@gmail.com>
2020-03-29 16:15:14 +0800

    leetcode 309: solved

---
+++ leetcode-clion/dynamic_programming/stock/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=309 lang=cpp
+ *
+ * [309] Best Time to Buy and Sell Stock with Cooldown
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+ *
+ * algorithms
+ * Medium (45.04%)
+ * Total Accepted:    108.7K
+ * Total Submissions: 241.4K
+ * Testcase Example:  '[1,2,3,0,2]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (ie, buy one and sell one share of the stock
+ * multiple times) with the following restrictions:
+ *
+ *
+ * You may not engage in multiple transactions at the same time (ie, you must
+ * sell the stock before you buy again).
+ * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1
+ * day)
+ *
+ *
+ * Example:
+ *
+ *
+ * Input: [1,2,3,0,2]
+ * Output: 3
+ * Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ *
+ */
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        int dp_i_0 = 0, dp_i_1 = INT_MIN;
+        int dp_pre_0 = 0; // dp[i-2][0]
+        for (int i = 0; i < n; ++i) {
+            int tmp = dp_i_0; // dp[i-1][0]
+            dp_i_0 = max(
+                         dp_i_0,            // rest
+                         dp_i_1 + prices[i] // sell
+                     );
+            dp_i_1 = max(
+                         dp_i_1,              // rest
+                         dp_pre_0 - prices[i] // buy
+                     );
+            dp_pre_0 = tmp;
+        }
+        return dp_i_0;
+    }
+};
