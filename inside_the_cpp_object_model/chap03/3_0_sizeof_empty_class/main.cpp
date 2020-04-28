081c03d5b4aad3befc66a70ce34daf0bfaa8e30f
lightjameslyy <lightjameslyy@gmail.com>
2020-04-28 17:03:51 +0800

    icom: 3.0 update

--- inside_the_cpp_object_model/chap03/3_0_sizeof_empty_class/main.cpp
+++ inside_the_cpp_object_model/chap03/3_0_sizeof_empty_class/main.cpp
@@ -27,6 +27,28 @@ void test() {
 }
 } // namespace test_3_0_1
 
+namespace test_3_0_2 {
+
+struct A {};
+struct B : public virtual A {};
+struct C : public virtual A {};
+struct D : public B, public C {
+    virtual void d() {}
+};
+
+void test() {
+    A a;
+    B b;
+    C c;
+    D d;
+
+    printf("sizeof A: %d\n", sizeof(A));
+    printf("sizeof B: %d\n", sizeof(B));
+    printf("sizeof C: %d\n", sizeof(C));
+    printf("sizeof D: %d\n", sizeof(D));
+}
+} // namespace test_3_0_2
+
 int main(int argc, char* argv[]) {
 
     string hostName, Ip;
@@ -39,6 +61,10 @@ int main(int argc, char* argv[]) {
     printf("test_3_0_1 +++\n");
     test_3_0_1::test();
     printf("test_3_0_1 ---\n");
+
+    printf("test_3_0_2 +++\n");
+    test_3_0_2::test();
+    printf("test_3_0_2 ---\n");
 
     auto end = system_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
